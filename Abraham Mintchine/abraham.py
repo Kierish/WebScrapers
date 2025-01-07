7ad4bfec804b6c6b6db05bcfd3d0351bcd575954
s498809 <kirkav2@st.amu.edu.pl>
2025-01-07 16:53:35 +0100

    asdf

--- Abraham Mintchine/abraham.py
+++ Abraham Mintchine/abraham.py
@@ -1,3 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -6,6 +10,23 @@ import os
 
 main_url = "https://mintchinesociety.org/?page_id=1388"
 script_dir = os.path.dirname(__file__)
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Запуск без GUI
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+# Укажите путь к драйверу Chrome (chromedriver).
+# При работе в docker-контейнере можно устанавливать в Dockerfile.
+driver_path = "/usr/bin/chromedriver"
+service = Service(driver_path)
+
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+
+driver.get(main_url)
+html = driver.page_source
+driver.quit()
 
 response = requests.get(main_url)
 soup = BeautifulSoup(response.text, 'html.parser')
