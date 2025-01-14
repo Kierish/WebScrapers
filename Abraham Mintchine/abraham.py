e30e7a3fcb258c94ae155f16fffa57f03a311dff
s498809 <kirkav2@st.amu.edu.pl>
2025-01-14 22:34:54 +0100

    refined Docker Abraham

--- Abraham Mintchine/abraham.py
+++ Abraham Mintchine/abraham.py
@@ -27,6 +27,8 @@ main_url = "https://mintchinesociety.org/?page_id=1388"
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu") 
+chrome_options.add_argument("--no-sandbox") 
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920x1080") 
 service = Service()
 driver = webdriver.Chrome(service=service, options=chrome_options)
